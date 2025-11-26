9ba1302911c872ca295f4009293dbfa247dbe53f
yuta.nagano <yuta.nagano2000@gmail.com>
2025-11-26 15:11:26 +0900

    Ginのチュートリアルを実装

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Specialz"},
+	{ID: "2", Title: "IRIS OUT"},
+	{ID: "3", Title: "Lemon"},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
 }
